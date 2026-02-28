1f2ccd14981d366bc5cc6b504dec639e25995d42
phuoclv20 <phuoclv@secomus.com>
2026-02-28 09:29:14 +0700

    fix viteconfig

--- project-fe/vite.config.js
+++ project-fe/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/app/',
+  build: {
+    outDir: '../project-be/public/app',
+    emptyOutDir: true,
+  },
 })
